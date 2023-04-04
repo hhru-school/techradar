ed9dd2d7289ea574abf9da5b43a7d16aef11cac7
Nikita Maslennikov <nikitax160@yandex.ru>
2023-04-04 13:32:02 +0300

    linter added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-
-import './App.css';
+import styles from './app.module.less';
 
 function App() {
-  return <div className="App"></div>;
+    return <div className={styles.app}></div>;
 }
 
 export default App;
