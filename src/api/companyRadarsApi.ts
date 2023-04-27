36880e67c1ea46734868a29383fdc7de50d660d8
Nikita Maslennikov <nikitax160@yandex.ru>
2023-04-28 00:07:30 +0300

    update rtk query

--- src/api/companyRadarsApi.ts
+++ src/api/companyRadarsApi.ts
@@ -1,5 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+import { ApiRadarData } from './radarApiUtils';
+
 const baseUrl = '/api/radars';
 
 export interface RadarApi {
@@ -7,14 +9,22 @@ export interface RadarApi {
     name: string;
 }
 
+// Все радары компании:
+// http://localhost:8080/api/radars?companyId=1
+// Конкретный радар:
+// http://localhost:8080/api/radars/1
+
 export const companyRadarsApi = createApi({
     reducerPath: 'companyRadarsApi',
     baseQuery: fetchBaseQuery({ baseUrl }),
     endpoints: (builder) => ({
-        getCompanyRadars: builder.query<RadarApi[], number>({
+        getAllCompanyRadars: builder.query<RadarApi[], number>({
             query: (companyId) => `?companyId=${companyId}`,
+        }),
+        getRadar: builder.query<ApiRadarData, number>({
+            query: (radarId) => `/${radarId}`,
         }),
     }),
 });
 
-export const { useGetCompanyRadarsQuery } = companyRadarsApi;
+export const { useGetAllCompanyRadarsQuery, useGetRadarQuery } = companyRadarsApi;
