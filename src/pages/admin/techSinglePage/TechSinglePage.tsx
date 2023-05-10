e4c156b0c017baf09213267f170cc2d6d165d1fe
unknown <ugin.bro@ya.ru>
2023-05-10 15:23:49 +0300

    add inputs in single page

--- src/pages/admin/techSinglePage/TechSinglePage.tsx
+++ src/pages/admin/techSinglePage/TechSinglePage.tsx
@@ -1,6 +1,6 @@
 import { FC, useState } from 'react';
 import EditIcon from '@mui/icons-material/Edit';
-import { Box, Button, Container, Grid, TextField, Typography } from '@mui/material';
+import { Box, Button, Container, Divider, Grid, TextField, Typography } from '@mui/material';
 
 const boxLogStyle = {
     boxSizing: 'border-box',
@@ -22,13 +22,15 @@ const TechSinglePage: FC = () => {
 
     return (
         <Container maxWidth="xl">
-            <Typography variant="h5" sx={{ textAlign: 'left', margin: '15px 0 0 40px' }}>
+            <Typography variant="h5" sx={{ textAlign: 'left', margin: '15px 0 15px 40px' }}>
                 Название технологии
             </Typography>
+            <Divider />
             <Grid container spacing={2} sx={{ mt: '5px' }}>
                 <Grid
                     item
-                    xs={6}
+                    xs={12}
+                    md={6}
                     sx={{
                         maxHeight: '85vh',
                     }}
@@ -76,28 +78,80 @@ const TechSinglePage: FC = () => {
                         }}
                         multiline
                         placeholder="Внесите информацию о технологии..."
-                        maxRows={15}
+                        maxRows={10}
                         sx={{ width: '100%' }}
                     />
+                    <Grid container>
+                        <Grid item xs={6}>
+                            <Typography variant="h6">Есть в радарах:</Typography>
+                            <Box
+                                sx={{
+                                    boxSizing: 'border-box',
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    width: '98%',
+                                    bgcolor: 'background.paper',
+                                    position: 'relative',
+                                    overflow: 'auto',
+                                    maxHeight: '20vh',
+                                    borderRadius: '5px',
+                                    padding: '15px 15px',
+                                    border: '1px solid',
+                                    borderColor: 'rgba(0, 0, 0, 0.23)',
+                                }}
+                            >
+                                {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, index) => (
+                                    <Button key={index} variant="contained" color="primary" sx={{ mb: 1 }}>
+                                        Frontend
+                                    </Button>
+                                ))}
+                            </Box>
+                        </Grid>
+                        <Grid item xs={6} sx={{ display: 'flex', flexDirection: 'column', alignItems: 'end' }}>
+                            <Box sx={{ width: '98%' }}>
+                                <Typography variant="h6">Отказались от использования:</Typography>
+                                <Box
+                                    sx={{
+                                        boxSizing: 'border-box',
+                                        display: 'flex',
+                                        flexDirection: 'column',
+                                        width: '100%',
+                                        bgcolor: 'background.paper',
+                                        position: 'relative',
+                                        overflow: 'auto',
+                                        maxHeight: '20vh',
+                                        borderRadius: '5px',
+                                        padding: '15px 15px',
+                                        border: '1px solid',
+                                        borderColor: 'rgba(0, 0, 0, 0.23)',
+                                    }}
+                                >
+                                    {[1, 1].map((_, index) => (
+                                        <Button key={index} variant="contained" color="primary" sx={{ mb: 1 }}>
+                                            Backend
+                                        </Button>
+                                    ))}
+                                </Box>
+                            </Box>
+                        </Grid>
+                    </Grid>
                 </Grid>
                 <Grid
                     item
-                    xs={6}
+                    xs={12}
+                    md={6}
                     sx={{
-                        maxHeight: '85vh',
+                        maxHeight: '80vh',
                     }}
                 >
-                    <Typography variant="h5" sx={{ textAlign: 'center' }}>
+                    <Typography variant="h5" sx={{ textAlign: 'center', height: '36px' }}>
                         Лог изменений
                     </Typography>
                     <Box sx={boxLogStyle}>
                         {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, index) => (
                             <Button key={index} sx={{ borderBottom: '1px solid', marginBottom: '10px' }}>
                                 <Typography>
-                                    <strong>
-                                        11/04/2023 <br />
-                                        20:22
-                                    </strong>
+                                    <strong>Дата: 11/04/2023 Время: 20:22 Радар: Frontend</strong>
                                     <br />
                                     Добавил преимущество, изменил ссылку на доку
                                 </Typography>
