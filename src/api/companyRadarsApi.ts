21528e5c508cbc0440838aa9bf4585d7cb4cfeec
Nikita Maslennikov <nikitax160@yandex.ru>
2023-05-19 17:35:42 +0300

    hooks refactor

--- src/api/companyRadarsApi.ts
+++ src/api/companyRadarsApi.ts
@@ -4,7 +4,7 @@ import { RootState } from '../store/store';
 import {
     ApiRadarData,
     CreateRadarApiData,
-    CreateRadarApiDataResponse,
+    RadarApiDataResponse,
     CreateRadarVersionDataApi,
     CreateRadarVersionDataApiResponse,
     FormattedRadarData,
@@ -22,8 +22,12 @@ export interface RadarApi {
 // http://localhost:8080/api/radars?companyId=1
 // Конкретный радар:
 // http://localhost:8080/api/radars/1
+
 // создание радара за один раз: /api/containers
 // Создать новую версию для радара POST на /api/radar_versions
+// Получить все версии радара: GET на api/radar_versions?radarId=1
+// Получение радара определенной версии (по blipEventId): GET на api/containers?blipEventId=77
+// Получить лог радара: GET на api/blip_events/radar_log?blipEventId=78
 
 export const companyRadarsApi = createApi({
     reducerPath: 'companyRadarsApi',
@@ -32,9 +36,40 @@ export const companyRadarsApi = createApi({
         getAllCompanyRadars: builder.query<RadarApi[], number>({
             query: (companyId) => `?companyId=${companyId}`,
         }),
+        // getRadar: builder.query<FormattedRadarData, number>({
+        //     query: (radarId) => `/${radarId}`,
+        //     transformResponse: (rawResult: ApiRadarData) => formatApiData(rawResult),
+        // }),
+
         getRadar: builder.query<FormattedRadarData, number>({
-            query: (radarId) => `/${radarId}`,
+            query: (blipEventId) => `/containers?blipEventId=${blipEventId}`,
             transformResponse: (rawResult: ApiRadarData) => formatApiData(rawResult),
+        }),
+
+        // getRadar: builder.query<FormattedRadarData, number>({
+        //     query: (radarId) => `/${radarId}`,
+        //      transformResponse: (rawResult: ApiRadarData) => formatApiData(rawResult),
+        //  }),
+
+        // getRadar: builder.query<FormattedRadarData, RadarQueryParams>({
+        //     async queryFn(arg, _api, _options, fetchBaseQuery) {
+        //         if (arg.blipEventId) {
+        //             const radar = await fetchBaseQuery(`/containers?blipEventId=${arg.blipEventId}`);
+        //             if (radar.error) return { error: radar.error as FetchBaseQueryError };
+        //             return radar.data as FormattedRadarData;
+        //         }
+
+        //         const radarVersions = await fetchBaseQuery({
+        //             url: `/${arg.radarId}`,
+        //             method: 'GET',
+        //         });
+        //         if (radarVersions.error) return { error: radarVersions.error };
+
+        //     },
+        // }),
+
+        getAllRadarVersions: builder.query<CreateRadarVersionDataApiResponse[], number>({
+            query: (radarId) => `radar_versions?radarId=${radarId}`,
         }),
 
         saveNewRadar: builder.mutation<CreateRadarVersionDataApiResponse, CreateRadarApiData>({
@@ -45,7 +80,7 @@ export const companyRadarsApi = createApi({
                     body: radarData,
                 });
                 if (radarResponse.error) return { error: radarResponse.error };
-                const radar = radarResponse.data as CreateRadarApiDataResponse;
+                const radar = radarResponse.data as RadarApiDataResponse;
                 const versionRequestBody: CreateRadarVersionDataApi = {
                     name: (getState() as RootState).editRadar.radarVersion,
                     release: false,
@@ -66,4 +101,5 @@ export const companyRadarsApi = createApi({
     }),
 });
 
-export const { useGetAllCompanyRadarsQuery, useGetRadarQuery, useSaveNewRadarMutation } = companyRadarsApi;
+export const { useGetAllCompanyRadarsQuery, useGetRadarQuery, useGetAllRadarVersionsQuery, useSaveNewRadarMutation } =
+    companyRadarsApi;
