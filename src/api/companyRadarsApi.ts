881c5750f4e0f9b774ad81e161e71178d458a166
Nikita Maslennikov <nikitax160@yandex.ru>
2023-05-19 13:25:05 +0300

    mutation

--- src/api/companyRadarsApi.ts
+++ src/api/companyRadarsApi.ts
@@ -1,8 +1,17 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-import { ApiRadarData, FormattedRadarData, formatApiData } from './radarApiUtils';
+import { RootState } from '../store/store';
+import {
+    ApiRadarData,
+    CreateRadarApiData,
+    CreateRadarApiDataResponse,
+    CreateRadarVersionDataApi,
+    CreateRadarVersionDataApiResponse,
+    FormattedRadarData,
+    formatApiData,
+} from './radarApiUtils';
 
-const baseUrl = '/api/radars';
+const baseUrl = '/api/';
 
 export interface RadarApi {
     id: number;
@@ -13,6 +22,8 @@ export interface RadarApi {
 // http://localhost:8080/api/radars?companyId=1
 // Конкретный радар:
 // http://localhost:8080/api/radars/1
+// создание радара за один раз: /api/containers
+// Создать новую версию для радара POST на /api/radar_versions
 
 export const companyRadarsApi = createApi({
     reducerPath: 'companyRadarsApi',
@@ -25,7 +36,34 @@ export const companyRadarsApi = createApi({
             query: (radarId) => `/${radarId}`,
             transformResponse: (rawResult: ApiRadarData) => formatApiData(rawResult),
         }),
+
+        saveNewRadar: builder.mutation<CreateRadarVersionDataApiResponse, CreateRadarApiData>({
+            async queryFn(radarData, { getState }, _options, fetchBaseQuery) {
+                const radarResponse = await fetchBaseQuery({
+                    url: 'containers',
+                    method: 'POST',
+                    body: radarData,
+                });
+                if (radarResponse.error) return { error: radarResponse.error };
+                const radar = radarResponse.data as CreateRadarApiDataResponse;
+                const versionRequestBody: CreateRadarVersionDataApi = {
+                    name: (getState() as RootState).editRadar.radarVersion,
+                    release: false,
+                    radarId: radar.radarId,
+                    blipEventId: radar.blipEventId,
+                };
+                const result = await fetchBaseQuery({
+                    url: 'radar_versions',
+                    method: 'POST',
+                    body: versionRequestBody,
+                });
+
+                if (result.error) return { error: result.error };
+
+                return { data: result.data as CreateRadarVersionDataApiResponse };
+            },
+        }),
     }),
 });
 
-export const { useGetAllCompanyRadarsQuery, useGetRadarQuery } = companyRadarsApi;
+export const { useGetAllCompanyRadarsQuery, useGetRadarQuery, useSaveNewRadarMutation } = companyRadarsApi;
