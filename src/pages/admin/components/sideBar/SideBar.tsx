842604bf95e0c6ca2dbd6e03d3c4a7f6fec6aa2a
unknown <ugin.bro@ya.ru>
2023-05-22 12:11:41 +0300

    some fix

--- src/pages/admin/components/sideBar/SideBar.tsx
+++ src/pages/admin/components/sideBar/SideBar.tsx
@@ -6,27 +6,16 @@ import LogList from '../logList/LogList';
 const SideBar: FC = () => {
     const [open, setOpen] = useState(false);
 
-    const toggleDrawer = useCallback(
-        (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
-            if (
-                event.type === 'keydown' &&
-                ((event as React.KeyboardEvent).key === 'Tab' || (event as React.KeyboardEvent).key === 'Shift')
-            ) {
-                return;
-            }
-
-            setOpen(open);
-        },
-        []
-    );
+    const handleClick = useCallback(() => setOpen(true), []);
+    const handleClose = useCallback(() => setOpen(false), []);
 
     return (
         <div>
-            <Button onClick={toggleDrawer(true)} variant="contained" color={'primary'}>
+            <Button onClick={handleClick} variant="contained" color={'primary'}>
                 Лог событий
             </Button>
 
-            <Drawer anchor={'right'} open={open} onClose={toggleDrawer(false)}>
+            <Drawer anchor={'right'} open={open} onClose={handleClose}>
                 <LogList boxWidth="250px" boxMaxHeight="88vh" />
             </Drawer>
         </div>
