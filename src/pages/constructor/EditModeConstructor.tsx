353a7c27ab4fc899a46f7ad71cb68d1a7fe574aa
Nikita Maslennikov <nikitax160@yandex.ru>
2023-05-22 16:20:45 +0300

    constructor slug refactor

---
+++ src/pages/constructor/EditModeConstructor.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const EditModeConstructor: FC = () => {
+    return (
+        <>
+            <h1>Edit mode</h1>
+        </>
+    );
+};
+
+export default EditModeConstructor;
