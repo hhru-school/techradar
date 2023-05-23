a2050d86d7543b7e3d392f5e36bdd9b4db4b8e55
Nikita Maslennikov <nikitax160@yandex.ru>
2023-05-24 02:12:30 +0300

    dispatcher

--- src/pages/constructor/Constructor.tsx
+++ src/pages/constructor/Constructor.tsx
@@ -4,6 +4,7 @@ import { ConstructorMode, setEditMode } from '../../store/editRadarSlice';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import MainContainer from './MainContainer';
 import MainEditPanel from './editPanel/MainEditPanel';
+import { useCurrentRadar } from './hooks';
 import Modals from './modals/Modals';
 import ModeDispatcher from './modeDispatchers/ModeDispatcher';
 
@@ -18,7 +19,6 @@ const Constructor: FC<Props> = ({ mode = ConstructorMode.NewRadarCreation }) => 
         dispatch(setEditMode(mode));
     }, [dispatch, mode]);
 
-const Constructor: FC<Props> = ({ mode = 'new' }) => {
     return (
         <>
             <Modals />
