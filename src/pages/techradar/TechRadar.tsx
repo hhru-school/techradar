bd2f89c384559b6dd3c57d3872faa5dcfb835622
nikita160 <nikitax160@yandex.ru>
2023-06-02 18:39:00 +0300

    version selection

--- src/pages/techradar/TechRadar.tsx
+++ src/pages/techradar/TechRadar.tsx
@@ -4,12 +4,15 @@ import { skipToken } from '@reduxjs/toolkit/dist/query';
 
 import {
     useGetAllCompanyRadarsQuery,
+    useGetAllRadarVersionsQuery,
     useGetLastReleasedVersionQuery,
     useGetRadarByVersionIdQuery,
+    useGetVersionByIdQuery,
 } from '../../api/companyRadarsApi';
 import { isFetchBaseQueryError } from '../../api/helpers';
 import ErrorMessage from '../../components/error/ErrorMessage';
 import TechRadarMain from './components/main/TechRadarMain';
+import SelectVersion from './components/selectMenu/SelectVersion';
 import SelectRadarPanel from './components/selectRadarPanel/SelectRadarPanel';
 
 export interface Version {
@@ -36,6 +39,10 @@ const TechRadar: FC = () => {
         error: radarError,
     } = useGetRadarByVersionIdQuery(versionId ?? skipToken);
 
+    const { data: versions } = useGetAllRadarVersionsQuery(radarId, { skip: !radar });
+
+    const { data: currentVersion } = useGetVersionByIdQuery(versionId ?? skipToken);
+
     if (radarError) {
         return <ErrorMessage errorStatus={isFetchBaseQueryError(radarError) ? radarError.status : null} />;
     }
@@ -43,7 +50,9 @@ const TechRadar: FC = () => {
     return (
         <>
             {radars && <SelectRadarPanel radars={radars} companyId={Number(companyId)} />}
-
+            {versions && currentVersion && radar && (
+                <SelectVersion versions={versions} version={currentVersion} companyId={1} radarId={radar.id} />
+            )}
             {<TechRadarMain radar={radar} isLoading={radarIsFetching} />}
         </>
     );
