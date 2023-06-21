c003c6e2be7470b160f51309d50beb9790de9400
nikita160 <nikitax160@yandex.ru>
2023-06-22 01:59:38 +0300

    controls color

--- src/pages/techradar/components/radar/controls/SectorControlPanel.tsx
+++ src/pages/techradar/components/radar/controls/SectorControlPanel.tsx
@@ -1,17 +1,16 @@
 import { FC } from 'react';
 
-import { defaultColorScheme } from '../../../../../components/radar/styleConfig';
 import { Sector } from '../../../../../components/radar/types';
 import SectorControlChip from './SectorControlChip';
 
 import styles from './controls.module.less';
 
-type Props = { sectors: Sector[]; colorScheme?: string[] };
+type Props = { sectors: Sector[] };
 
-const SectorControlPanel: FC<Props> = ({ sectors, colorScheme = defaultColorScheme }) => {
-    const chips = sectors.map((sector, i) => (
-        <SectorControlChip key={sector.id} sector={sector} color={colorScheme[i]} />
-    ));
+const color = 'rgb(215, 215, 215)';
+
+const SectorControlPanel: FC<Props> = ({ sectors }) => {
+    const chips = sectors.map((sector) => <SectorControlChip key={sector.id} sector={sector} color={color} />);
 
     return <div className={styles.sectorControlContainer}>{chips}</div>;
 };
